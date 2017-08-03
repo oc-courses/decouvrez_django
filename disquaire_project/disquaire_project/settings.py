1e81a0c40708d90fa0aa0a03f8aeec8e66faada4
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-08-03 16:23:11 +0200

    add app to settings

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'debug_toolbar',
+    'store.apps.StoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
