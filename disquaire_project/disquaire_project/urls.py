0e2631d7df79212abe6b309a68d94a84974047a0
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-08-09 19:25:07 +0200

    no namespace and urls

--- disquaire_project/disquaire_project/urls.py
+++ disquaire_project/disquaire_project/urls.py
@@ -21,7 +21,7 @@ from store import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^store/', include('store.urls', namespace='store')),
+    url(r'^store/', include('store.urls')),
     url(r'^admin/', admin.site.urls)
 ]
 
