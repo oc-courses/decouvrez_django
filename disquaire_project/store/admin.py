b38dfc8a4f77575f083da72c0ec71691949fdae4
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-08-13 18:18:30 +0200

    P5C3 ok

--- disquaire_project/store/admin.py
+++ disquaire_project/store/admin.py
@@ -1,20 +1,52 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
 
 from .models import Booking, Contact, Album, Artist
 
 
-@admin.register(Booking)
-class BookingAdmin(admin.ModelAdmin):
-    list_filter = ['created_at', 'contacted']
+class AdminURLMixin(object):
+    def get_admin_url(self, obj):
+        content_type = ContentType.objects.get_for_model(obj.__class__)
+        return reverse("admin:store_%s_change" % (
+            content_type.model),
+            args=(obj.id,))
 
-class BookingInline(admin.TabularInline):
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin, AdminURLMixin):
+    list_filter = ['created_at', 'contacted']
+    fields = ["created_at", "contact_link", 'album_link', 'contacted']
+    readonly_fields = ["created_at", "contact_link", "album_link", "contacted"]
+
+    def has_add_permission(self, request):
+        return False
+
+    def contact_link(self, booking):
+        url = self.get_admin_url(booking.contact)
+        return mark_safe("<a href='{}'>{}</a>".format(url, booking.contact.name))
+
+    def album_link(self, booking):
+        url = self.get_admin_url(booking.album)
+        return mark_safe("<a href='{}'>{}</a>".format(url, booking.album.title))
+
+class BookingInline(admin.TabularInline, AdminURLMixin):
     model = Booking
     extra = 0
-    fieldsets = [
-        (None, {'fields': ['album', 'contacted']})
-        ]
+    readonly_fields = ["created_at", "album_link", "contacted"]
+    fields = ["created_at", "album_link", "contacted"]
     verbose_name = "Réservation"
     verbose_name_plural = "Réservations"
+
+    def has_add_permission(self, request):
+        return False
+
+    def album_link(self, booking):
+        url = self.get_admin_url(booking.album)
+        return mark_safe("<a href='{}'>{}</a>".format(url, booking.album.title))
+
+    album_link.short_description = "Album"
 
 
 @admin.register(Contact)
